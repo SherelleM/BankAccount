559a8aeb1d9ac05756c9ebb6cf37728df04f3b50
Sherelle McDaniel <sherelle.r.a.mcdaniel@gmail.com>
2026-02-10 09:28:06 -0500

    chore: add initial BankAccount files

---
+++ bank.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    bank_title = "Bank"
+
+    def __init__(self, customer_name, current_balance, minimum_balance):
+        self.customer_name = customer_name
+        self.current_balance = current_balance
+        self.minimum_balance = minimum_balance
+
+    def deposit(self, deposit_amount):
+        self.current_balance += deposit_amount
+        print(f"Successfully deposited {deposit_amount}.")
+        print(f"Current balance: {self.current_balance}")
+
+    def withdraw(self, withdraw_amount):
+        if self.current_balance - withdraw_amount < self.minimum_balance:
+            print(f"Cannot withdraw less than minimum balance of {self.minimum_balance}.")
+            print(f"Current balance: {self.current_balance}")
+        else:
+            self.current_balance -= withdraw_amount
+            print(f"Successfully withdrawn {withdraw_amount}")
+            print(f"Current balance: {self.current_balance}")
+
+    def print_customer_information(self):
+        print(f"Bank Title: {self.bank_title}")
+        print(f"Customer Name: {self.customer_name}")
+        print(f"Current Balance: {self.current_balance}")
+
